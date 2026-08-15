8ed9417c69458910ef070ad14244e461a60099ee
vgxg <chloeclapton3@gmail.com>
2026-08-15 06:58:01 +0530

    fix vercel deployement with nitro

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [tanstackStart(), viteReact(), tailwindcss()],
+  plugins: [tanstackStart(), viteReact(), tailwindcss(), nitro()],
 });
